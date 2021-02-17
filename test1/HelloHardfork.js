95a516b0422c0d926866690754f5eefb50682cae
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-17 18:01:22 +0100

    commit

---
+++ test1/HelloHardfork.js
@@ -0,0 +1,6 @@
+let xor = ('A' !== 'B')
+console.log(xor)
+
+
+
+
---
+++ test1/exercices-1-js/Hello.js
@@ -0,0 +1,18 @@
+let msg1 = 'HelloHardfork'
+let msg2 = 'GoodbyeHardfork'
+let space = ' '
+
+
+
+
+let sum = msg1 + ' ' + msg2
+console.log(sum)
+let sum2 = msg1 + space + msg2
+console.log(sum2)
+
+
+
+let intro = 'Hello'
+let prenom = 'Victor'
+let msg = (intro) 'de la part de'(prenom)
+console.log(msg)
