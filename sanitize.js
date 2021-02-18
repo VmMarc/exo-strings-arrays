968873933b96cbebabc6a8cf8e6390617178862d
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-18 10:41:28 +0100

    typo

--- sanitize.js
+++ sanitize.js
@@ -2,9 +2,5 @@ const msg = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs 
 
 let msg1 = msg.trim()
 let msg2 = msg1.toUpperCase()
-let msg3 = msg2.split(` `)
-let msg4 = msg3.forEach(function (item) {
-  if (item === msg3) {
-    console.log(msg4)
-  }
-})
+
+console.log(msg2)
