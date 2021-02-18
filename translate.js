4f874111f6bd9dcb23a2b18ada272867e9a88a6e
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-18 14:45:23 +0100

    update

--- translate.js
+++ translate.js
@@ -2,6 +2,16 @@ const msg = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs 
 
 let sanitizeMsg = msg.trim()
 let sanitizeMsg2 = sanitizeMsg.toUpperCase()
-let sanitizeMsg3 = sanitizeMsg2.forEach(fonction(item))
-console.log(`mot: ${item}`)
+let splitMsg = sanitizeMsg2.split(` `)
 
+//Boucle let elem of ...
+for (let elem of splitMsg) {
+  console.log(`mot: ` + elem)
+}
+
+//Boucle .forEach
+/*
+splitMsg.forEach((elem) => {
+  console.log(`mot: ` + elem)
+})
+*/
