c86402172aa3ef7851a5f52e2d67150404be50dd
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-18 10:52:19 +0100

    update

--- findBob.js
+++ findBob.js
@@ -1,3 +1,4 @@
+/*
 const msg = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
 
 let sanitizeMsg = msg.trim()
@@ -14,3 +15,18 @@ for (let i = 0; i < splitMsg.length; i++)
   if (splitMsg[i] === `BOB`)
     count++
 console.log(`Il y a ${count} foix le nom BOB dans ce texte`)
+*/
+
+const msg = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
+
+let msg1 = msg.trim()
+let msg2 = msg1.toUpperCase()
+
+console.log(msg2)
+
+if (msg2.includes(`BOB`)) {
+  console.log(`Bob est bien dans ce texte`)
+} else {
+  console.log(`Bob n'est pas dans ce texte`)
+}
+
