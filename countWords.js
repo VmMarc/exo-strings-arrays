5f4d856b49b59cd36cbe15ebe6ae5be77dc994a4
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-18 10:34:39 +0100

    update

--- countWords.js
+++ countWords.js
@@ -1,27 +1,22 @@
-const msg = ('Je suis le ténébreux, - le veuf, - l\'inconsolé,' +
-  'Le prince d\'Aquitaine à la tour abolie :' +
-  'Ma seule étoile est morte, - et mon luth constellé' +
-  'Porte le soleil noir de la Mélancolie.' +
+const msg = (`Je suis le ténébreux, - le veuf, - l'inconsolé,
+  Le prince d'Aquitaine à la tour abolie :
+  Ma seule étoile est morte, - et mon luth constellé
+  Porte le soleil noir de la Mélancolie.
 
-  'Dans la nuit du tombeau, toi qui m\'as consolé,' +
-  'Rends-moi le Pausilippe et la mer d\'Italie,' +
-  'La fleur qui plaisait tant à mon cœur désolé,' +
-  'Et la treille où le pampre à la rose s\'allie.' +
+  Dans la nuit du tombeau, toi qui m'as consolé,
+  Rends-moi le Pausilippe et la mer d'Italie,
+  La fleur qui plaisait tant à mon cœur désolé,
+  Et la treille où le pampre à la rose s'allie.
 
-  'Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?' +
-  'Mon front est rouge encor du baiser de la reine ;' +
-  'J\'ai rêvé dans la grotte où nage la sirène...' +
+  Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?
+  Mon front est rouge encor du baiser de la reine ;
+  J'ai rêvé dans la grotte où nage la sirène...
+  
+  Et j'ai deux fois vainqueur traversé l'Achéron ;
+  Modulant tour à tour sur la lyre d'Orphée
+  Les soupirs de la sainte et les cris de la fée.`)
 
-  'Et j\'ai deux fois vainqueur traversé l\'Achéron ;' +
-  'Modulant tour à tour sur la lyre d\'Orphée' +
-  'Les soupirs de la sainte et les cris de la fée.')
+msg1 = msg.replaceAll('\n', '')
+let msg2 = msg1.split(' ');
 
-const splitMsg = msg.split(` `);
-console.log(splitMsg)
-
-let count = 0
-
-for (let i = 0; i < splitMsg.length; i++)
-  if (splitMsg[i])
-    count++
-console.log(`Il y a ${count} mots dans ce texte`)
+console.log(`Il y a ${msg2.length} mots dans ce texte`)
